9c40f752164f5d70ce08ccaaef335dd489e0ba09
as-capabl <as.capabl@gmail.com>
2019-03-31 15:48:08 +0900

    Move to new-build

---
+++ aux-list-path/src/aux-list-path.hs
@@ -0,0 +1,59 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE ViewPatterns #-}
+
+module
+    Main
+where
+
+import Control.Monad (guard)    
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import Data.Text.Lazy.Builder (toLazyText)
+import Data.Text.Lazy (toStrict)
+import qualified Data.Map.Strict as Map
+import System.Environment (getArgs)
+import System.Exit (die)
+import Cabal.Plan
+
+main :: IO ()
+main =
+  do
+    args <- getArgs
+    if length args < 2 then die_ usageStr else return ()
+    let [jsonPath, action] = args
+
+    PlanJson {pjUnits} <- decodePlanJson jsonPath
+
+    unit <-
+      do
+        let l = 
+              do
+                (_, u)   <- Map.toList pjUnits
+                (cn, ci) <- Map.toList $ uComps u
+                guard $ cn == CompNameFLib "clay-dhall"
+                return u 
+        case l
+          of
+            u:_ -> return u
+            []  -> die_ "No configured foreign lib"
+            
+    case action
+      of
+        "dist-dir" ->
+            maybe
+                (die_ $ "`clay-dhall` isn't a local package." )
+                putStrLn $
+                    uDistDir unit
+        _ ->
+            die_ $ "Unknown action `" <> T.pack action <> "`\n"
+
+die_ :: T.Text -> IO a
+die_ = die . T.unpack
+
+usageStr :: T.Text
+usageStr = toStrict . toLazyText . mconcat $ [
+    "usage: aux-list-path <path to plan.json> <action>\n",
+    "  actions\n",
+    "  - dist-dir: show dist directory\n"
+  ]
