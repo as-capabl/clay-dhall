71958f148c88fda9e89e3ca5bea75f5cd294e4ad
as-capabl <as.capabl@gmail.com>
2019-04-19 00:01:26 +0900

    type_input_simple.c

---
+++ ctest/type_input_simple.c
@@ -0,0 +1,20 @@
+#include <assert.h>
+#include <string.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <clay-dhall/clay-dhall.h>
+#include <clay-dhall/macro.h>
+
+int main()
+{
+    cdhall_init(NULL, NULL);
+
+    cdhall_uint n = 30;
+    cdhall_objptr exprN1 = cdhall_embed(CDHALL_TPTR_SIMPLE(&n));
+    cdhall_objptr exprN2 = cdhall_input_expr("30");
+    assert(cdhall_expr_eq(exprN1, exprN2));
+    cdhall_free_object(exprN1);
+    cdhall_free_object(exprN2);
+
+    cdhall_exit();
+}
